0bb61f457526c5893531929547e895c85a38379f
Larissa321 <larissa.negrao.25@gmail.com>
2021-06-28 16:39:40 -0300

    Alterado app.tssx e add index.tsx

---
+++ src/pages/Cadastro.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, TextInput, StyleSheet, Text } from 'react-native';
+
+import {ButtonCinza} from '../components/Button';
+
+export function Cadastro(){
+
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+ )
+};
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import {Cadastro} from '../pages/Cadastro'
+
 const stackRoutes = createStackNavigator();
 
 const AppRoutes: React.FC = () => (
     <stackRoutes.Navigator
     headerMode='none'
     >
+
+    <stackRoutes.Screen
+        name="Cadastro"
+        component={Cadastro}
+    />
     </stackRoutes.Navigator>
 )
 
