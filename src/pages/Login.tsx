96c9a0dcd6a2b75a35080e669011e39d40f367e3
pedroGNF <50996658+pedroGNF@users.noreply.github.com>
2021-06-30 18:57:46 -0300

    atualização

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,3 @@
-// Instalar comandos a baixo e depois dar "yarn start" ou "expo start"
-
-// { npm install --save react-native-vector-icons }
-
 import React from 'react';
 import {
   Text,
@@ -11,100 +7,109 @@ import {
   Image,
   TextInput,
   TouchableOpacity,
-  Button,
+  Platform,
+  ScrollView,
 } from 'react-native';
-import {useNavigation} from '@react-navigation/core';
 
-export default function Login() {
-
-  const navigation = useNavigation(); 
-
-  function handleStart(){
-    navigation.navigate('Menu')
-  }
+export default function App() {
 
     return (
-      <KeyboardAvoidingView style={styles.background}>
-        <View style={styles.header}>
-          <Text style={styles.textfooter}>VR ALZHEIMER</Text>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+        style={styles.background}>
+        {/* <ScrollView style={{flex: 1}}>   */}
+        <View style={styles.viewlogo}>
+         
+        <Image
+             style={styles.imagee}
+             source={require('./src/assets/VR_design.png')} />
+          <Text style={styles.vrtext}> VR ALZHEIMER </Text>
         </View>
-        
 
-        <View style={styles.body}>
-      
+        <View style={styles.conteiner}>
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            autoCorrect={false}
+            onChangeText={() => {}}
+          />
+
+          <TextInput
+            style={styles.input}
+            placeholder="Senha"
+            autoCorrect={false}
+            secureTextEntry={true}
+            onChangeText={() => {}}
+          />
+
           <TouchableOpacity style={styles.btnsubmit}>
-
-          
-
-            <Text style={styles.textsubmit}> Novo Paciente </Text>
-           
-            
+            <Text style={styles.textsubmit} onPress={() => { props.navigation.navigate("Menu") }}> Entrar </Text>
           </TouchableOpacity>
 
-          <TouchableOpacity style={styles.btnsubmit}  onPress={handleStart}>
-
-            <Image
-             style={styles.image2}
-             source={require('./src/assets/Lista.png')} />
-
-            <Text style={styles.textsubmit}> Lista de Pacientes </Text>
-            
+          <TouchableOpacity>
+            <Text style={styles.textt}> Criar Conta </Text>
           </TouchableOpacity>
         </View>
+        {/* </ScrollView> */}
       </KeyboardAvoidingView>
     );
 }
 
 const styles = StyleSheet.create({
   background: {
+    flex: 1,
     alignItems: 'center',
+    justifyContent: 'center',
     backgroundColor: '#0087E2',
-    height: '100%',
+  },
+  viewlogo: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  conteiner: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '90%',
+  },
+  input: {
+    backgroundColor: '#fff',
+    width: '90%',
+    height: '15%',
+    marginBottom: 15,
+    color: '#222',
+    fontSize: 17,
+    borderRadius: 20,
+    padding: 10,
   },
   btnsubmit: {
-    backgroundColor: '#FFFFFF',
-    width: 250,
-    height: 180,
+    backgroundColor: '#E59021',
+    width: '90%',
+    height: '15%',
     borderRadius: 20,
     alignItems: 'center',
-    padding: 50,
-    marginTop: 75,
-    shadowColor: 'rgba(0,0,0, .4)', // IOS
-    shadowOffset: { height: 2, width: 2 }, // IOS
-    shadowOpacity: 1, // IOS
-    shadowRadius: 1, //IOS
-  },
-  textsubmit: {
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  header: {
-    backgroundColor: '#00009C',
-    padding: 20,
-    width: '100%',
-    alignItems: 'center',
-  },
-  textfooter: {
-    color: 'white',
-    fontSize: 25,
-  },
-  body: {
-    alignItems: 'center',
-    height: '80%',
     justifyContent: 'center',
   },
-
-  image1: {
-    height: 50,
-    width: 50,
-    alignItems: 'center',
-    marginRight: 10,
+  textsubmit: {
+    color: '#fff',
+    fontSize: 18,
   },
-
-  image2: {
-    height: 50,
-    width: 50,
-    marginTop: 5,
+  imagee: {
+    height: 94,
+    width: 102,
+    marginTop: 60,
     alignItems: 'center',
-  }
-})
+  },
+  textt: {
+    marginTop: 10,
+    marginLeft: 100,
+    color: 'white',
+  },
+  vrtext: {
+    padding: 10,
+    fontSize: 30,
+    color: 'white',
+  },
+});
