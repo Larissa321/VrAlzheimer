e5587ec27568fde41730ec37f8417fabf59a2a18
mathibc <mathibc97@hotmail.com>
2021-10-17 16:22:50 -0300

    Update Paciente.tsx

--- src/pages/Paciente.tsx
+++ src/pages/Paciente.tsx
@@ -5,15 +5,16 @@
 // { yarn add react-native-paper }
 
 
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import { StyleSheet, Text, View, Alert, TouchableOpacity, ScrollView,Image} from 'react-native';
-import { WebView } from 'react-native-webview';
+import YoutubePlayer from "react-native-youtube-iframe";
 import { Card} from 'react-native-paper';
 import { LinearGradient } from 'expo-linear-gradient';
 import {useNavigation} from '@react-navigation/core';
 import logo from '../assets/VR_design.png'
 import back from '../assets/back.png'
 import question from '../assets/question.png'
+
 
 export default function Paciente() {
 
@@ -39,6 +40,8 @@ export default function Paciente() {
     ],
     { cancelable: false }
   );
+
+
   const showAlert = () =>{
     Alert.alert(
        'Para espelhar a tela em Android baixe: Screen Mirror. Para espelhar a tela em IOS baixe: Mirror MacPC.'
@@ -46,9 +49,7 @@ export default function Paciente() {
  }
  
 const remote = 'https://i.pinimg.com/originals/54/27/10/542710e5150ee4f7340bea644f6c75f0.jpg';
-  {/*Render não implementado corretamento preciso de auxilio*/}
-  render();{
-    return (
+  return (
     <View style={styles.telaPrincipal}>
       <LinearGradient
           colors={['rgba(2, 24, 88, 0.4)', '#0091F2']}
@@ -59,107 +60,99 @@ const remote = 'https://i.pinimg.com/originals/54/27/10/542710e5150ee4f7340bea64
             top: 0,
             height: 400,
           }}
-        /> 
+        />
 
-    <ScrollView style={styles.scrollview}> 
-    <View>
-    <TouchableOpacity onPress={handleStart2} style={styles.back}>
-    <Image source={back} />
-    </TouchableOpacity>
-    <TouchableOpacity onPress={alert} style={styles.question}>
-    <Image source={question} />
-    </TouchableOpacity>
-    </View>
-    
+    <ScrollView style={styles.scrollview}>
+
+
     <View style={styles.viewlogo}>
     <Image source={logo} />
     </View>
+
      <View style={styles.telaTitulo}>
-     <Text style={styles.usuario}>Pedro Fonseca</Text> {/*Get name Paciente*/}
-     <Text style={styles.info}>65 anos</Text> {/*Get idade Paciente*/}
-     <Text style={styles.info}>Eubasico</Text> {/*Get incapacidade Paciente*/}
-     <Text style={styles.info}>Alzheimer Moderado</Text> {/*Get grau Alzheimer Paciente*/}
-     
+     <Text style={styles.usuario}>Pedro Fonseca</Text>
      <TouchableOpacity>
-      <Text style={styles.verPerfil}>Mais informações</Text>
+      <Text style={styles.verPerfil}>ver perfil</Text>
      </TouchableOpacity> 
-
-      <TouchableOpacity onPress = {showAlert} style = {styles.evolucoesAnteriores}>
-       <Text style={styles.evolucoesText}>Evoluções Anteriores</Text>
+      <TouchableOpacity onPress = {showAlert} style = {styles.comoUsar}>
+       <Text>Como usar?</Text>
       </TouchableOpacity>
-
+    </View>
+    
+    <View style={styles.telaVideo}>
+     <Text style={styles.usuario}>Valsa Venesiana</Text>
+     <Text style={styles.info}>2 minutos</Text>
+    </View>
+    <View style={styles.video}>
+      <YoutubePlayer
+        play={false}
+        height={200}
+        videoId={"3sM3yVV-Yrg"}
+      />
     </View>
 
-    <View style={styles.telaTitulo}>
-       
-       <Text style={styles.usuario}>Valsa Venesiana</Text> {/*Get name Paciente*/}
-       <Text style={styles.info}>2 minutos</Text> {/*Get idade Paciente*/}
-       <WebView
-          source={{html: '<iframe src=" https://www.youtube.com/embed/3sM3yVV-Yrg?start=11"  title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;fullscreen" allowfullscreen>> </iframe>'}}
-       />
-
-      </View>
-
-      <View style={styles.telaTitulo}>
-       <Text style={styles.usuario}>Caminhada na Floresta</Text> {/*Get name Paciente*/}
-       <Text style={styles.info}>8 minutos</Text> {/*Get idade Paciente*/}
-       <WebView
-          source={{html: '<iframe src="https://www.youtube.com/embed/OkWVZa8mwog?start=11"  title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;fullscreen" allowfullscreen>> </iframe>'}}
-       />
-      </View>
+    <View style={styles.telaVideo}>
+     <Text style={styles.usuario}>Caminhada na Floresta</Text>
+     <Text style={styles.info}>8 minutos</Text> 
+    </View>
+    
+    <View style={styles.video}>
+      <YoutubePlayer
+        play={false}
+        height={200}
+        videoId={"iee2TATGMyI"}
+      />
+    </View>
        </ScrollView>
-       
+
        <View style={styles.footer}>
+            
+                   
+       
             <TouchableOpacity 
                style={styles.voltar}
                onPress={handleStart2}>
                <Text style={styles.assistirTexto}>VOLTAR</Text>
              </TouchableOpacity>
+     
 
+      
               <TouchableOpacity 
                  style={styles.evolucao}
                  onPress={handleStart}>
                  <Text style={styles.assistirTexto}>EVOLUÇÃO</Text>
               </TouchableOpacity>
+
+      
+        
+  
          </View>
+
    </View>
   );
 }
-}
-
 
 const styles = StyleSheet.create({
   telaPrincipal: {
    flex: 1,
    backgroundColor: '#0091F2',
    alignItems: 'center',
-  }, 
+  },
+
   viewlogo: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-   
+    marginTop: 35,
   },
-  back: {
-    flex: 1,
-    alignSelf:'flex-start',
-    marginLeft:20,
-    marginTop: 20,
-    width: 20,
-    height: 20,
-   
-  },
-  question: {
-    flex: 1,
-    alignSelf: 'flex-end',
-    width: 20,
-    height: 20,  
-  },
+
   usuario: {
-    fontSize: 20,
+    fontSize: 30,
     color: 'black',
     fontWeight:'bold',
+    width:300,
    },
+
    info: {
     fontSize: 14,
     color: 'black',
@@ -167,17 +160,29 @@ const styles = StyleSheet.create({
    },
 
    telaTitulo: {
+    
     backgroundColor: 'white',
     marginTop: 35,
     marginBottom: 1,
-    marginRight:10,
-    marginLeft: 10,
+    marginRight:20,
     padding: 40,
     borderRadius: 20,
-
+    width:320,
    },
 
-  comoUsar: {
+   telaVideo: {
+    backgroundColor: 'white',
+    marginTop: 30,
+    marginRight:20,
+    padding: 20,
+    width:320,
+   },
+
+   video: {
+    marginBottom:5
+   },
+
+   comoUsar: {
     backgroundColor: '#4ba37a',
     width: 100,
     borderRadius: 50,
@@ -185,22 +190,10 @@ const styles = StyleSheet.create({
     marginTop: 15,
     marginLeft: 150,
  },
- evolucoesAnteriores: {
-  width: 160,
-  alignItems: 'center',
-  marginTop: 15,
-  marginLeft: 90,
-},
-evolucoesText: {
-  color: '#4ba37a',
-  alignItems: 'center',
-  marginLeft: 20,
-  fontWeight: 'bold',
-},
 
  verPerfil: {
-  fontSize: 12,
-  color:'#6192d3',  
+  fontSize: 25,
+  color:'#4a8ea8',  
   },
 
   linearGradient: {
@@ -210,6 +203,7 @@ evolucoesText: {
     marginTop: 16,
     width: 350,
   },
+
   buttonText: {
     fontSize: 18,
     fontFamily: 'Gill Sans',
@@ -222,7 +216,6 @@ evolucoesText: {
   cardsTexto: {
     fontSize: 85,
     marginLeft: 10,
-    marginRight:10,
     fontWeight:'bold',
  
    },
@@ -259,20 +252,19 @@ evolucoesText: {
 
    cards: {
     fontSize: 25,
-    marginTop: 35,
-    marginBottom: 1,
-    marginRight:10,
-    marginLeft: 10,
-    padding: 40,
-    borderRadius: 20,
-    justifyContent:'space-between',
-    
-
+    marginLeft: 10.5,
+    width: 300,
+    height: 250,
+    marginBottom: 150,
+    marginTop: 60,
+    borderRadius: 25,
+    justifyContent:'space-between'
    },
 
    scrollview: {
-      flex: 1,
-  },
+    width: 320,
+    height: 200,
+   },
 
    footer: {
     color: '#0080FF',
